abce1dc18cb0fc09e16e0b6e02d06df8774b9f69
Giuseppe Acito <giuseppe.acito@gmail.com>
2021-04-17 10:02:40 +0200

    when adding colliders, be sure to activate them

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,8 +6,10 @@ public class PlayerController : MonoBehaviour
 {
   // Start is called before the first frame update
 
-  public float moveSpeed = 10;
-  public Animator animator;
+  [SerializeField] float moveSpeed = 10;
+  [SerializeField] private Animator animator;
+  [SerializeField] private Rigidbody2D body;
+
   void Start() { }
 
     // Update is called once per frame
@@ -15,11 +17,11 @@ public class PlayerController : MonoBehaviour
     float horizontalInput = Input.GetAxis("Horizontal");      
     float verticalInput = Input.GetAxis("Vertical");
 
+    Debug.Log("DIocane");
+
     Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0);
-    bool is_moving = velocity.magnitude > 0.01;
+    bool is_moving = velocity.magnitude > Mathf.Epsilon;
     animator.SetBool("Running", is_moving);
-    if (is_moving) {
-      transform.Translate(velocity * moveSpeed * Time.deltaTime);
-    }
+    if (is_moving) transform.Translate(velocity * moveSpeed * Time.deltaTime);
   }
 }
