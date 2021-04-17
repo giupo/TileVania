ca7be9021f683cd1717d01c80524b40efd8fea15
Giuseppe Acito <giuseppe.acito@gmail.com>
2021-04-17 10:37:47 +0200

    Oh My God he's Jason Bourne

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,27 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+
+public class PlayerController : MonoBehaviour {
   // Start is called before the first frame update
 
   [SerializeField] float moveSpeed = 10;
-  [SerializeField] private Animator animator;
-  [SerializeField] private Rigidbody2D body;
+  
+  private Animator animator;
+  private Rigidbody2D body;
 
-  void Start() { }
+  void Start() { 
+    animator = GetComponent<Animator>();
+    body = GetComponent<Rigidbody2D>();
+  }
 
-    // Update is called once per frame
-  void Update(){
+  // Update is called once per frame
+  void Update() {
+    animator.SetBool("Running", is_moving());
+  }
+
+  void FixedUpdate() {
     float horizontalInput = Input.GetAxis("Horizontal");      
     float verticalInput = Input.GetAxis("Vertical");
 
-    Debug.Log("DIocane");
+    if (horizontalInput > 0) FlipRight();
+    if (horizontalInput < 0) FlipLeft();
 
-    Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0);
-    bool is_moving = velocity.magnitude > Mathf.Epsilon;
-    animator.SetBool("Running", is_moving);
-    if (is_moving) transform.Translate(velocity * moveSpeed * Time.deltaTime);
+    Vector2 direction = new Vector2(horizontalInput, verticalInput);
+    Debug.Log(direction);
+    if (direction.magnitude > Mathf.Epsilon) {
+      body.velocity = direction * moveSpeed;
+    }
+  }
+
+  void FlipLeft() {
+    Vector3 scale = transform.localScale;
+    scale.x = -1 * Mathf.Abs(scale.x);
+    transform.localScale = scale;
+  }
+
+  void FlipRight() {
+    Vector3 scale = transform.localScale;
+    scale.x = Mathf.Abs(scale.x);
+    transform.localScale = scale;
+  }
+
+  bool is_moving() {
+    return body.velocity.magnitude > Mathf.Epsilon;
   }
 }
